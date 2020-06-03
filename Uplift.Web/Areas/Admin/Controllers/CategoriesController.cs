f97503623704a8f6371cf4e772b05e43b9b4b32d
Pankaj Jangid <pankaj.jangid>
2020-06-03 19:36:41 +0530

    Added categories controller and index view

---
+++ Uplift.Web/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uplift.Web.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
